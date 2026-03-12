5e99b8fdbbb1557e2b9d8cd413e5658cbae1e1a2
Natiq7855 <natabbasli@gmail.com>
2026-03-12 12:36:16 +0000

    SDF

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    if n < 0: return "undefined (negative)"
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-# Iterate directly over the slice
-for arg in sys.argv[1:]:
-    if arg.isdigit():
-        num = int(arg)
-        print(f"Factorial of {num} is {factorial(num)}")
-    else:
-        print(f"Skipping '{arg}': Not a positive integer")
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
