a6350567b8b9a1a6ec5cf4bf23a083c2643a9c37
Natiq7855 <natabbasli@gmail.com>
2026-03-12 12:31:51 +0000

    SDF

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-for arg in sys.argv:
-    print(arg)
+def factorial(n):
+    if n < 0: return "Error"
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# We use sys.argv[1:] to skip the script name itself
+for arg in sys.argv[1:]:
+    try:
+        num = int(arg)
+        print(f"Factorial of {num} is {factorial(num)}")
+    except ValueError:
+        print(f"'{arg}' is not a valid number, skipping.")
